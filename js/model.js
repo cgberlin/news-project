334575847585694c600b7009e578e3e124e34b5f
cgberlin <codyberlin@gmail.com>
2016-08-10 22:42:53 -0700

    finished landing pages

--- js/model.js
+++ js/model.js
@@ -1,11 +1,27 @@
 function animateLanding(){
   $('#landing-title').addClass('animated rollIn');
+  $('#news-img-env').addClass('animated lightSpeedIn');
 }
 
 $('.landing-page-container').on('click', '#landing-title', function(){
+  $('#news-img-env').removeClass('animated lightSpeedIn').addClass('animated rotateOutDownRight');
   $('#landing-title').removeClass('animated rollIn').addClass('animated hinge').delay(2000).queue(function(next) {
     $('#landing-title').hide();
     $('#landing-credits').addClass('animated rollIn').show();
+    $('#white-star1').addClass('animated zoomInDown').show().delay(300).queue(function(next) {
+      $('#white-star2').addClass('animated zoomInDown').show().delay(200).queue(function(next) {
+        $('#white-star3').addClass('animated zoomInDown').show();
+          next();
+        next();
     next();
+    });
+    });
   });
+});
+
+$('.landing-page-container').on('click', '#landing-credits', function(){
+  $('#landing-credits').removeClass('animated rollIn').addClass('animated zoomOutUp');
+  $('#white-star1').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
+  $('#white-star2').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
+  $('#white-star3').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
 });
