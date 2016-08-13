5475b90805f0692f514c71103636b445d4a926b2
cgberlin <codyberlin@gmail.com>
2016-08-13 11:45:58 -0700

    added info to markers on click and map zoom

--- js/model.js
+++ js/model.js
@@ -1,11 +1,9 @@
 var map;
 
-
 function initMap() {
   map = new google.maps.Map(document.getElementById('google-map'), {
     center: {lat: 37.09024, lng: -95.712891},
     zoom: 1
-
   });
 }
 
@@ -21,14 +19,7 @@ function getNews(searchedTerm){
       xhr.setRequestHeader("X-Mashape-Authorization", "i7QyNxw44UmshFKsRu1bVZikhxFSp1e22YAjsnqtPgrHNbANrX"); // Enter here your Mashape key
       }
   });
-
 }
-
-
-
-
-
-
 
 function animateLanding(){
   $('#landing-title').addClass('animated rollIn');
@@ -78,17 +69,24 @@ function processData(data){
   $.each(data, function(index, obj){
     console.log(data);
     console.log(obj.geometry.coordinates);
-    var headingFromNews = 'asdasdasda';
+    var headingFromNews = obj.properties.title;
+    var detailFromNews = obj.properties.description;
+    var dateFromNews = obj.properties.date;
+    var sourceFromNews = obj.properties.source;
+    var urlFromNews = obj.properties.url;
     var contentString = '<div id="content">'+
         '<div id="siteNotice">'+
         '</div>'+
         '<h1 id="firstHeading" class="firstHeading">'+headingFromNews+'</h1>'+
+        '<p>Details: '+detailFromNews+'</p>'+
+        '<p>Date: '+dateFromNews+'</p>'+
+        '<p>source: '+sourceFromNews+'</p>'+
+        '<p><a href = " '+urlFromNews+'">'+urlFromNews+'</a></p>'+
         '</div>'+
         '</div>';
 
     var latitude = obj.geometry.coordinates[1];
     var longitude = obj.geometry.coordinates[0];
-
     var infowindow = new google.maps.InfoWindow({
                       content: contentString
                     });
@@ -97,5 +95,10 @@ function processData(data){
                   map: map,
                   title: 'News'
                 });
+                marker.addListener('click', function() {
+                  map.setZoom(8);
+                  map.setCenter(marker.getPosition());
+                  infowindow.open(map, marker);7
+            });
   });
 }
