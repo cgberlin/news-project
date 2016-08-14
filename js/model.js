b5c3b04bd34b3448295aa450945d3a106fff827a
cgberlin <codyberlin@gmail.com>
2016-08-13 18:41:21 -0700

    moved some stuff over to control.js

--- js/model.js
+++ js/model.js
@@ -21,49 +21,7 @@ function getNews(searchedTerm){
   });
 }
 
-function animateLanding(){
-  $('#landing-title').addClass('animated rollIn');
-  $('#news-img-env').addClass('animated lightSpeedIn');
-}
 
-$('.landing-page-container').on('click', '#landing-title', function(){
-  $('#news-img-env').removeClass('animated lightSpeedIn').addClass('animated rotateOutDownRight');
-  $('#landing-title').removeClass('animated rollIn').addClass('animated hinge').delay(2000).queue(function(next) {
-    $('#landing-title').hide();
-    $('#landing-credits').addClass('animated rollIn').show();
-    $('#white-star1').addClass('animated zoomInDown').show().delay(300).queue(function(next) {
-      $('#white-star2').addClass('animated zoomInDown').show().delay(200).queue(function(next) {
-        $('#white-star3').addClass('animated zoomInDown').show();
-          next();
-        next();
-    next();
-    });
-    });
-  });
-});
-
-$('.landing-page-container').on('click', '#landing-credits', function(){
-  $('#landing-credits').removeClass('animated rollIn').addClass('animated zoomOutUp');
-  $('#white-star1').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
-  $('#white-star2').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
-  $('#white-star3').removeClass('animated zoomInDown').addClass('animated zoomOutUp');
-  callInMap();
-});
-
-$('.landing-page-container').on('click', '#search-button', function(){
-  var searchedTerm = $('#search-term').val();
-  getNews(searchedTerm);
-});
-
-function callInMap(){
-  $('#google-map').addClass('animated rollIn').show();
-  $('#about-button').addClass('animated rollIn').show();
-  $('#search-button').addClass('animated rollIn').show();
-  $('#prompt-user-search').addClass('animated rollIn').show();
-  $('#search-term').addClass('animated rollIn').show();
-  initMap();
-  map.setMapTypeId('satellite');
-}
 
 function processData(data){
   $.each(data, function(index, obj){
